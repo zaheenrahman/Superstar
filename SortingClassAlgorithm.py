22db8007dd32287295f1cbb9c1bc41bfb5e17094
ismaeelBM <35469226+ismaeelBM@users.noreply.github.com>
2018-05-24 20:49:45 -0600

    Update SortingClassAlgorithm.py

--- SortingClassAlgorithm.py
+++ SortingClassAlgorithm.py
@@ -16,5 +16,5 @@ assignment1_272 = Task(datetime.date(2018,5,27), 90, 'CMPUT272','high',7)
 lab1_272 = Task(datetime.date(2018,5,30), 90, 'CMPUT272','high',3)
 lab1_229 = Task(datetime.date(2018,5,30), 600, 'CMPUT272','high',5)
 
-task_list = [assignment272, assignment229]
+task_list = [assignment1_272, assignment1_229, lab1_229, lab1_272]
 
